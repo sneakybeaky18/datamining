0d905940c253cccf7c0797698f66102058335aa7
sneakybeaky18 <sneakybeaky18@gmail.com>
2020-10-22 21:51:51 +0300

    KONSKAYA ZALUPA NIXYA NE RABOTAET

--- geek_brains_parser__.py
+++ geek_brains_parser__.py
@@ -2,11 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import uuid
+import pymongo
+from pymongo import MongoClient
 
 class Parser_GeekBrains:
 
     def __init__(self, start_url):
         self.start_url = start_url
+        mongo_client = MongoClient('mongodb://localhost:27017')
+        self.db = mongo_client['geek_brains_parser']
 
     def get_response(self):
         self.response = requests.get(self.start_url)
@@ -16,9 +20,10 @@ class Parser_GeekBrains:
         self.soup = BeautifulSoup(self.get_response().text, 'lxml')
         return self.soup
 
-    def dict_name_generator(self):
-        self.number = uuid.uuid4()
-        return self.number
+
+    def save_to(self, product_data):
+        collection = self.db['geek_brains']
+        collection.insert_one(product_data)
 
     def parse(self):
         geek_brains_post_structure = {
@@ -35,20 +40,23 @@ class Parser_GeekBrains:
             'text_comment': '',
         }
 
+        def uuid_gen():
+            uuidGen = uuid.uuid4()
+            return uuidGen
+
         self.material_without_url = self.get_soup().find_all('a', attrs={'class': 'post-item__title h3 search_text'})
         list_of_url = []
         for self.material_with_url in self.material_without_url:
             self.material_with_url = self.material_with_url.get('href')
+            # geek_brains_post_structure['material_url'] = self.material_with_url
             list_of_url.append(self.material_with_url)
+            # self.save_to(geek_brains_post_structure)
 
-            for el in list_of_url:
-                geek_brains_post_structure['material_url'] = el
-                print(geek_brains_post_structure)
+        for el in list_of_url:
+            geek_brains_post_structure['material_url'] = el
+            self.save_to(geek_brains_post_structure)
+
         return geek_brains_post_structure
-
-
-            # print(self.material_with_url)
-
 
 
     
@@ -57,7 +65,3 @@ pg = Parser_GeekBrains("https://geekbrains.ru/posts")
 print('hi')
 print(pg.parse())
 print(pg.get_response())
-print(pg.dict_name_generator())
-# print(pg.get_soup())
-
-# print(requests.get('https://geekbrains.ru/posts'))
